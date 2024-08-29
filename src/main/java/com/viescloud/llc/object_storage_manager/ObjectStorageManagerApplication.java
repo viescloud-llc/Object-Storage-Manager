d585db86703d02b73db0dd4c8ca3052395fa5027
VincentB00 <vincentbui1999@gmail.com>
2024-08-29 10:43:17 -0400

    init

---
+++ src/main/java/com/viescloud/llc/object_storage_manager/ObjectStorageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.object_storage_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStorageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectStorageManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viescloud/llc/object_storage_manager/ObjectStorageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.object_storage_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStorageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
