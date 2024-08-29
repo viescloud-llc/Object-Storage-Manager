a9ba7861f829cb40a1b8e7fbb7a82eecfe80ed12
VincentB00 <vincentbui1999@gmail.com>
2024-08-29 16:37:10 -0400

    add boiler plate for service

--- src/test/java/com/viescloud/llc/object_storage_manager/ObjectStorageManagerApplicationTests.java
+++ src/test/java/com/viescloud/llc/object_storage_manager/ObjectStorageManagerApplicationTests.java
@@ -1,13 +1,5 @@
 package com.viescloud.llc.object_storage_manager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ObjectStorageManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
